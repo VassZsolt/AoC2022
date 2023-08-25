76e84288584633f6e6ce340247589d5568ff6029
Vass Zsolt <vass.zsolt@nexius.hu>
2023-08-25 15:53:33 +0200

    Solve day 01 part 2

--- AoC22.Test/Day01/Day01Tests.cs
+++ AoC22.Test/Day01/Day01Tests.cs
@@ -33,4 +33,33 @@ public sealed class Day01Tests
         // Assert
         result.Should().Be("24000");
     }
+
+    [Fact]
+    public void Part2_Example_from_website()
+    {
+        // Arrange 
+        string input = """
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
+            """;
+        Day01Solver solver = new Day01Solver();
+
+        // Act
+        string result = solver.SolvePart2(input);
+
+        // Assert
+        result.Should().Be("45000");
+    }
 }
--- AoC22/Day01/Day01Solver.cs
+++ AoC22/Day01/Day01Solver.cs
@@ -4,7 +4,8 @@ public sealed class Day01Solver
 {
     public string SolvePart1(string inputFileContent)
     {
-        string[] caloriesList = inputFileContent.Split("\r\n");
+        List<string> caloriesList = inputFileContent.Split("\r\n").ToList();
+        caloriesList.Add(string.Empty);
 
         int sumOfCarryingCalories = 0, maxOfCarryingCalories = 0;
         foreach (string calories in caloriesList)
@@ -22,11 +23,40 @@ public sealed class Day01Solver
                 sumOfCarryingCalories = 0;
             }
         }
+
         return maxOfCarryingCalories.ToString();
     }
 
     public string SolvePart2(string inputFileContent)
     {
-        return "yoo2";
+        List<string> caloriesList = inputFileContent.Split("\r\n").ToList();
+        caloriesList.Add(string.Empty);
+
+        List<int> amountOfCarriedCalories = new List<int>();
+        int sumOfCarryingCalories = 0;
+
+        foreach (string calories in caloriesList)
+        {
+            if (calories != string.Empty)
+            {
+                sumOfCarryingCalories += int.Parse(calories);
+            }
+            else
+            {
+                amountOfCarriedCalories.Add(sumOfCarryingCalories);
+                sumOfCarryingCalories = 0;
+            }
+        }
+
+        amountOfCarriedCalories.Sort();
+        amountOfCarriedCalories.Reverse();
+
+        int sumOfTop3CarryingCalories = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            sumOfTop3CarryingCalories += amountOfCarriedCalories[i];
+        }
+
+        return sumOfTop3CarryingCalories.ToString();
     }
 }
