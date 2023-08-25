7d3f7838cd6fc40c72f99201847b45a624f4077b
Vass Zsolt <vass.zsolt@nexius.hu>
2023-08-25 15:24:30 +0200

    Solve day 01 part 1

--- AoC22.Test/Day01/Day01Tests.cs
+++ AoC22.Test/Day01/Day01Tests.cs
@@ -1,10 +1,36 @@
+using AoC22.Day01;
+using FluentAssertions;
+
 namespace AoC22.Test.Day01;
 
 public sealed class Day01Tests
 {
     [Fact]
-    public void Test1()
+    public void Part1_Example_from_website()
     {
-        // TODO
+        // Arrange 
+        string input = """
+            1000
+            2000
+            3000
+
+            4000
+
+            5000
+            6000
+
+            7000
+            8000
+            9000
+
+            10000
+            """;
+        Day01Solver solver = new Day01Solver();
+
+        // Act
+        string result = solver.SolvePart1(input);
+
+        // Assert
+        result.Should().Be("24000");
     }
 }
--- AoC22/Day01/Day01Solver.cs
+++ AoC22/Day01/Day01Solver.cs
@@ -4,7 +4,25 @@ public sealed class Day01Solver
 {
     public string SolvePart1(string inputFileContent)
     {
-        return "yoo1";
+        string[] caloriesList = inputFileContent.Split("\r\n");
+
+        int sumOfCarryingCalories = 0, maxOfCarryingCalories = 0;
+        foreach (string calories in caloriesList)
+        {
+            if (calories != string.Empty)
+            {
+                sumOfCarryingCalories += int.Parse(calories);
+            }
+            else
+            {
+                if (sumOfCarryingCalories > maxOfCarryingCalories)
+                {
+                    maxOfCarryingCalories = sumOfCarryingCalories;
+                }
+                sumOfCarryingCalories = 0;
+            }
+        }
+        return maxOfCarryingCalories.ToString();
     }
 
     public string SolvePart2(string inputFileContent)
