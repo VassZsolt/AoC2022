9f2f5999db8949badc7945bb027e9bb69f59a66a
Vass Zsolt <vass.zsolt@nexius.hu>
2023-09-03 20:40:46 +0200

    Initialize day02

---
+++ AoC22.Test/Day02/Day02Tests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC22.Test.Day02
+{
+    internal class Day02Tests
+    {
+        public void Part1_Example_from_website()
+        {
+        }
+
+        public void Part2_Example_from_website()
+        {
+        }
+    }
---
+++ AoC22/Day02/Day02Solver.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC22.Day02;
+
+internal class Day02Solver
+{
+    public string SolvePart1(string inputFileContent)
+    {
+    }
+
+    public string SolvePart2(string inputFileContent)
+    {
+    }
+
+}
