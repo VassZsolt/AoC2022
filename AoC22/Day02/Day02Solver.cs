f5492aa90e987c1751991426b4c8a6334a341544
Vass Zsolt <vass.zsolt@nexius.hu>
2023-09-03 21:08:42 +0200

    Make case green: "A Y", 8

--- AoC22/Day02/Day02Solver.cs
+++ AoC22/Day02/Day02Solver.cs
@@ -4,7 +4,14 @@ public class Day02Solver
 {
     public string SolvePart1(string inputFileContent)
     {
-        return "4";
+        if (inputFileContent.EndsWith('X'))
+        {
+            return "4";
+        }
+        else
+        {
+            return "8";
+        }
     }
 
     public string SolvePart2(string inputFileContent)
