d2c9ac792dcd2a2d4bcaa4123ee22b0251cbedaf
Vass Zsolt <vass.zsolt@nexius.hu>
2023-09-14 10:53:22 +0200

    Day03 Part1 separate the compartments

---
+++ AoC22/Day03/Day03Solver.cs
@@ -0,0 +1,33 @@
+﻿namespace AoC22.Day03;
+
+public class Day03Solver
+{
+    public string SolvePart1(string inputFileContent) // A prioritások össszege
+    {
+        string result = "";
+
+        List<string> rucksacks = inputFileContent
+            .Split("\r\n", StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+        foreach(string rucksack in rucksacks)
+        {
+            List<string> rucksackCompartments=GetRucksackCompartments(rucksack);            
+        }
+
+
+
+        return result;
+    }
+
+    private List<string> GetRucksackCompartments(string oneRucksack) // A rekeszek szétválasztása
+    {
+        List<string> result = new List<string>
+        {
+            oneRucksack.Substring(0, oneRucksack.Length / 2),
+            oneRucksack.Substring(oneRucksack.Length / 2)
+        };
+
+        return result;
+    }
+}
