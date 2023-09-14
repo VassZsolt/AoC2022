9b7926e04f7daee84cfa12ab2a758e2a107375f4
Vass Zsolt <vass.zsolt@nexius.hu>
2023-09-14 10:58:59 +0200

    Day03 Part1 determinate the shared item

--- AoC22/Day03/Day03Solver.cs
+++ AoC22/Day03/Day03Solver.cs
@@ -13,11 +13,22 @@ public class Day03Solver
         foreach(string rucksack in rucksacks)
         {
             List<string> rucksackCompartments=GetRucksackCompartments(rucksack);
+            char sharedItem=SharedItemBetweenCompartments(rucksackCompartments);
         }
         return result;
     }
 
-
+    private char SharedItemBetweenCompartments(List<string> rucksackCompartments) // A rekeszek közös tartalma
+    {
+        foreach(char item in rucksackCompartments[0])
+        {
+            if (rucksackCompartments[1].Contains(item))
+            {
+                return item;
+            }
+        }
+        return ' ';
+    }
 
     private List<string> GetRucksackCompartments(string oneRucksack) // A rekeszek szétválasztása
     {
