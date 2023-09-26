e26fce7183e1d1db6ae1466d750db1a74cc41476
Vass Zsolt <vass.zsolt@nexius.hu>
2023-09-26 19:45:13 +0200

    Day05 Test the new move logic for 1 and n crates

--- AoC22.Test/Day05/Day05Tests.cs
+++ AoC22.Test/Day05/Day05Tests.cs
@@ -5,7 +5,7 @@ namespace AoC22.Test.Day05;
 public class Day05Tests
 {
     [Fact]
-    public void VerifyWarehouseMoveMethod()
+    public void VerifyWarehouseMoveMethodQuantity1()
     {
         Warehouse warehouse = new Warehouse(
             new List<CrateTower>
@@ -32,7 +32,7 @@ public class Day05Tests
 
                     }),
             });
-        warehouse.MoveCrate(2, 1);
+        warehouse.MoveCrates(1,2, 1);
 
         Warehouse resultWarehouse = new Warehouse(
             new List<CrateTower>
@@ -56,6 +56,62 @@ public class Day05Tests
                     new List<Crate> // 3. oszlop
                     {
                         new Crate('P')
+                    }),
+             });
+
+        warehouse.Should().BeEquivalentTo(resultWarehouse);
+    }
+    [Fact]
+    public void VerifyWarehouseMoveMethodQuantityN()
+    {
+        Warehouse warehouse = new Warehouse(
+            new List<CrateTower>
+            {
+                new CrateTower(
+                    new List<Crate> // 1. oszlop
+                    {
+                        new Crate('Z'),
+                        new Crate('N')
+                    }),
+
+                new CrateTower(
+                    new List<Crate> // 2. oszlop
+                    {
+                        new Crate('M'),
+                        new Crate('C'),
+                        new Crate('D')
+                    }),
+
+                new CrateTower(
+                    new List<Crate> // 3. oszlop
+                    {
+                        new Crate('P')
+
+                    }),
+            });
+        warehouse.MoveCrates(2, 1, 3);
+
+        Warehouse resultWarehouse = new Warehouse(
+            new List<CrateTower>
+            {
+                new CrateTower(
+                    new List<Crate> // 1. oszlop
+                    {}),
+
+                new CrateTower(
+                    new List<Crate> // 2. oszlop
+                    {
+                        new Crate('M'),
+                        new Crate('C'),
+                        new Crate('D'),
+                    }),
+
+                new CrateTower(
+                    new List<Crate> // 3. oszlop
+                    {
+                        new Crate('P'),
+                        new Crate('Z'),
+                        new Crate('N')
                     }),
              });
 
