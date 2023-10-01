abb52c14b0d81fd6c373e0236fab41c10c078465
Vass Zsolt <vass.zsolt@nexius.hu>
2023-10-01 16:51:15 +0200

    Day06 Make the Part1 tests green

--- AoC22/Day06/Day06Solver.cs
+++ AoC22/Day06/Day06Solver.cs
@@ -4,7 +4,14 @@ public class Day06Solver
 {
     public string SolvePart1(string inputFileContent)
     {
-        return "";
+        for (int i = 0; i < inputFileContent.Length - 3; i++)
+        {
+            if (isValidIdentifier(inputFileContent.Substring(i, 4)))
+            {
+                return (i + 4).ToString();
+            }
+        }
+        return "Not found valid identifier";
     }
 
     public bool isValidIdentifier(string identifier)
@@ -15,7 +22,7 @@ public class Day06Solver
         }
         else
         {
-            int numberOfUniqueItems =identifier.ToCharArray()
+            int numberOfUniqueItems = identifier.ToCharArray()
                 .Distinct()
                 .Count();
 
