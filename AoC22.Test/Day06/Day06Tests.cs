0c34a2cdb4c9147dee486347a7e6dce51a5dd44c
Vass Zsolt <vass.zsolt@nexius.hu>
2023-10-01 16:29:45 +0200

    Day06 Create the solver class and the tests for the identifier validation.

---
+++ AoC22.Test/Day06/Day06Tests.cs
@@ -0,0 +1,33 @@
+﻿using AoC22.Day05;
+using AoC22.Day06;
+using FluentAssertions;
+
+namespace AoC22.Test.Day06;
+public class Day06Tests
+{
+    [Theory]
+    [InlineData("abc")] // Túl rövid
+    [InlineData("abcde")] // Túl hosszú
+    [InlineData("abca")] // Tartalmaz ismétlődést
+
+    public void ShouldBeInvalidIdentitfier(string identifier)
+    {
+        Day06Solver solver = new Day06Solver();
+        bool result = solver.isValidIdentifier(identifier);
+
+        result.Should().BeFalse();
+    }
+
+    [Theory]
+    [InlineData("abcd")] // Megfelő
+
+    public void ShouldBeValidIdentitfier(string identifier)
+    {
+        Day06Solver solver = new Day06Solver();
+        bool result = solver.isValidIdentifier(identifier);
+
+        result.Should().BeTrue();
+    }
+
+
+}
---
+++ AoC22/Day06/Day06Solver.cs
@@ -0,0 +1,19 @@
+﻿namespace AoC22.Day06;
+
+public class Day06Solver
+{
+    public string SolvePart1(string inputFileContent)
+    {
+        return "";
+    }
+
+    public bool isValidIdentifier(string identifier)
+    {
+        return true;
+    }
+
+    public string SolvePart2(string inputFileContent)
+    {
+        return "";
+    }
+}
